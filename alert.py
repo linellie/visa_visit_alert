0bcac32e6b8b9973d2ce8d5b329f977f7a41fdd1
linellie <yhlin002@outlook.com>
2025-02-05 21:09:46 -0700

    Update alert.py

--- alert.py
+++ alert.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import time
 from playsound import playsound
 
-##test
 # Function to play a sound
 def play_alert_sound():
     # Path to your sound file
@@ -12,8 +11,8 @@ def play_alert_sound():
     playsound(sound_file)
 
 # Aug 1 is fly to Portugal date
-trip_date = datetime(2024, 7, 25)  # What is the latest appointment you want? 
-sessions_token = "https://www.ch-edoc-reservation.admin.ch/#/session?token=XXXXXXXXXXXXXXX" # Obtain this link from the email directly
+trip_date = datetime(2025, 4, 29)  # What is the latest appointment you want? 
+sessions_token = "https://www.ch-edoc-reservation.admin.ch/#/session?token=9Z4gXVsj&locale=en-US" # Obtain this link from the email directly
 
 while True:
     try:
